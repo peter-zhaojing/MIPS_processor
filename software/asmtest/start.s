0d4c84d015d49c6d08aa169a44d5c896840b444e
peter-zhaojing <peter.zhaojing@gmail.com>
2014-03-01 23:10:20 -0800

    update .gitignore and remove more junk files

--- software/asmtest/start.s
+++ software/asmtest/start.s
@@ -4,10 +4,14 @@
 _start:
 
 # Test 1
-lw $t0, 0($0)
-lw $t1, 1($0)
-lw $t2, 2($0)
-lw $t3, 3($0)
+lw $t0, 268435456($0)		#read DMEM address0
+lw $t1, 2147483660($0)		#read IO
+lb $t2, 1($0)
+lbu $t3, 1($0)
+sw $t0, 268435464($0)		#sw DMEM address8
+sw $t0, 536870916($0)		#sw IMEM address4
+sw $t0,	805306380($0)		#sw IMEM+DMEM address12
+sw $t0, 2147483656($0)		#sw IO
 
 
 #li $s0, 0x00000020 addiu $t0, $0, 0x20
