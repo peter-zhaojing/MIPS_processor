be8739e27161509559e8dc8d3efcdd49714d0abf
peter-zhaojing <peter.zhaojing@gmail.com>
2014-03-01 22:44:49 -0800

    remove junk files

--- fuseRelaunch.cmd
+++
@@ -1,1 +0,0 @@
--intstyle "ise" -incremental -lib "unisims_ver" -lib "unimacro_ver" -lib "xilinxcorelib_ver" -lib "secureip" -o "D:/1OSU/berkeley_cs150/spring2013/mylab/MIPS_processor/MIPS_processor/BlkMemTestbench_isim_beh.exe" -prj "D:/1OSU/berkeley_cs150/spring2013/mylab/MIPS_processor/MIPS_processor/BlkMemTestbench_beh.prj" "work.BlkMemTestbench" "work.glbl" 
--- hardware/sim/work/.cxl.verilog.xilinxcorelib.xilinxcorelib_ver.nt.cmd
+++
@@ -1,3 +0,0 @@
-d:\Modeltech_pe_edu_10.2c\win32pe_edu/vlib d:\Xilinx\14.2\ISE_DS\ISE\verilog\mti_pe\10.2c\nt/xilinxcorelib_ver
-d:\Modeltech_pe_edu_10.2c\win32pe_edu/vmap xilinxcorelib_ver d:\Xilinx\14.2\ISE_DS\ISE\verilog\mti_pe\10.2c\nt/xilinxcorelib_ver
-d:\Modeltech_pe_edu_10.2c\win32pe_edu/vlog -source -32 -work xilinxcorelib_ver -f d:\Xilinx\14.2\ISE_DS\ISE\verilog\mti_pe\10.2c\nt/xilinxcorelib_ver/.cxl.verilog.xilinxcorelib.xilinxcorelib_ver.nt.cmf
